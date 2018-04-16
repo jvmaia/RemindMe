68da191e3c3cc7290df5df5fd917cb99aa8feb3f
João Vítor <joaovmferreira@gmail.com>
2018-04-16 20:21:50 -0300

    fix tag

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import tweepy
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-tag = '#rememberme'
+tag = '#remindme'
 
 def remember(text, tweetId, user):
     tweet = f'@{user} {text}'
